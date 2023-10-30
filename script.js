f92796f55489ca3e0a713a204bf0b42ca7d73f6c
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-30 12:02:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function sortArticles(bands) {
+	const ignoredWords = ['a', 'an', 'the'];
+
+	const customSort = (a, b) => {
+		a = a.replace(/^(a|an|the) /i, '');
+		b = b.replace(/^(a|an|the) /i, '');
+		return a.localeCompare(b);
+	};
+
+	bands.sort(customSort);
+
+	const bandList = document.getElementById('band');
+	bands.forEach(article => {
+		const listItem = document.createElement('li');
+		listItem.textContent = article;
+		bandList.appendChild(listItem);
+	});
+}
+
+sortArticles(bands);
