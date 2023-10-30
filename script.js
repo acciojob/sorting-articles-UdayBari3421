fb3f02cbbefd96f879f555122e74809c37e2d5ae
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-30 12:09:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-function sortArticles(bands) {
-	const ignoredWords = ['a', 'an', 'the'];
+const sortedBands = bands.sort((a, b) => {
+	const ignoreWords = ['a', 'an', 'the'];
+	const removeIgnoredWords = (str) => str.replace(/^(a|an|the) /i, '');
+	return removeIgnoredWords(a).localeCompare(removeIgnoredWords(b));
+});
 
-	const customSort = (a, b) => {
-		a = a.replace(/^(a|an|the) /i, '');
-		b = b.replace(/^(a|an|the) /i, '');
-		return a.localeCompare(b);
-	};
-
-	bands.sort(customSort);
-
-	const bandList = document.getElementById('bands');
-	bands.forEach(article => {
-		const listItem = document.createElement('li');
-		listItem.textContent = article;
-		bandList.appendChild(listItem);
-	});
-}
-
-sortArticles(bands);
+const bandList = document.getElementById('bands');
+sortedBands.forEach(article => {
+	const listItem = document.createElement('li');
+	listItem.textContent = article;
+	bandList.appendChild(listItem);
+});
